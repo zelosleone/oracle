afbd2ca5885440184f771c05ecfbeb94d267b440
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2024-11-23 21:46:27 +0300

    Update prayers.ts

--- src/utils/prayers.ts
+++ src/utils/prayers.ts
@@ -3,11 +3,12 @@ const PRAYERS = {
   hebrew: 'הכפף לי כל הדיימונים, למען ישמעו לי כל דיימון, בין בשמים או ברוח או בארץ או מתחת לארץ או ביבשה או במים, וכל קסם ונגע אשר מאת האלוהים'
 };
 
-export async function recitePrayer(type: keyof typeof PRAYERS) {
+export async function recitePrayer(type: keyof typeof PRAYERS, volume: number = 0.8) {
   if ('speechSynthesis' in window) {
     const utterance = new SpeechSynthesisUtterance(PRAYERS[type]);
     utterance.lang = type === 'greek' ? 'el-GR' : 'he-IL';
     utterance.rate = 0.8;
+    utterance.volume = volume;
     window.speechSynthesis.speak(utterance);
     
     return new Promise((resolve, reject) => {
